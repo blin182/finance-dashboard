484d2b7cbaaac491497ea8e5f59d28b6be53fe25
blin182 <172394995+blin182@users.noreply.github.com>
2025-05-31 22:04:32 +0200

    Implement authentication and login functionality with error handling

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
+// This matcher ensures that the middleware runs for all routes except API routes, static files, and image files.
+// The regex checks for any path that does not start with 'api', '_next/static', '_next/image', or ends with '.png'.
