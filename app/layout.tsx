46011443b2f70e3470dfcb50e39b9e082db7af20
blin182 <172394995+blin182@users.noreply.github.com>
2025-06-02 15:23:01 +0200

    Add metadata definitions for pages and update layout with dynamic title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,20 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Analytics } from '@vercel/analytics/next';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL(
+    process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : 'http://localhost:3000'
+  ),
+};
 
 export default function RootLayout({
   children,
