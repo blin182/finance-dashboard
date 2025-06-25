70dbce3ddcdb6c932a4edbd3f63c5bc3f161d4da
blin182 <172394995+blin182@users.noreply.github.com>
2025-06-25 16:30:18 +0200

    update login form labels and placeholders to include example values

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -32,7 +32,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="email"
             >
-              Email
+              Email (user@nextmail.com)
             </label>
             <div className="relative">
               <input
@@ -40,7 +40,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="(user@nextmail.com)Enter your email address"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -51,7 +51,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="password"
             >
-              Password
+              Password (123456)
             </label>
             <div className="relative">
               <input
@@ -59,7 +59,7 @@ export default function LoginForm() {
                 id="password"
                 type="password"
                 name="password"
-                placeholder="Enter password"
+                placeholder="(123456)Enter password"
                 required
                 minLength={6}
               />
