d322ff71a86058e4d0cbeffc21d75f083c7e465a
Ivan <172394995+blin182@users.noreply.github.com>
2025-12-23 12:00:51 +0100

    refactor: clean up next.config.ts and update tsconfig.json for improved readability and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during builds
-  },
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
